3f2861937fd0fd95c8458d253d7e686bb551230a
Varun Sukheja <varun.sukheja@here.com>
2021-07-16 06:57:45 +0530

    feat: created AddTimer component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
 import React from 'react';
 import styles from './App.module.scss';
+import AddTimer from './components/AddTimer';
 
 function App() {
-  return <div className={styles.app}> </div>;
+  const onAddTimer = (timerInSec: number) => {
+    console.log('timer to add', timerInSec);
+  };
+
+  return (
+    <div className={styles.app}>
+      <header>
+        <AddTimer onAddTimer={onAddTimer} />
+      </header>
+      <main></main>
+    </div>
+  );
 }
 
 export default App;
